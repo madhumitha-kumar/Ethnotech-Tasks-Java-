f8daa97bc63333f39f2e768bdb428448e8ffee7d
madhumitha-kumar <madhumithakumar021@gmail.com>
2026-02-11 10:29:27 +0530

    Add Car class and main method to print color

---
+++ 6.java
@@ -0,0 +1,15 @@
+package prg1;
+
+class Car {
+    String color;
+}
+
+public class classes {
+    public static void main(String[] args) {
+
+        Car c1 = new Car();
+        c1.color = "Red";
+
+        System.out.println(c1.color);
+    }
+}
